18b07a7aa7e567b7cfc4e588252dee31db02e0f3
StefanTan18 <stan3@stuy.edu>
2018-03-01 08:40:06 -0500

    added HW12

---
+++ HW12/go.kt
@@ -0,0 +1,22 @@
+Stefan Tan
+APCS2 pd1
+HW12 -- +3R|\/|1|\|4|_  1|_|_|\|3$$
+2018-03-01
+
+Q0: What do you make of the multiple class definitions in a single .java file?
+I believe the classes that don't have protection in its signature can only
+be used by the class that has a protection.
+
+Q1: How are the command line arguments processed?
+The command line take in the args as an array of Stringsseperated by the
+spaces. The arguments can be accessed performing methods on the array args. 
+
+Q2: What happens if no command line arguments are given?
+
+Q3: What is the nature/purpose of the recursion? Is it necessary for the
+animation?
+
+Q4: What do you expect to see when you run it?
+
+Q5: What does ANSI stand for? (You are permitted some intertubes searching
+here…). What is it good for?
