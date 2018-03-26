022f5bb50e17ebe622a381e456e64d3ee2029dcc
StefanTan18 <stan3@stuy.edu>
2018-03-26 04:37:58 -0400

    added HW25

---
+++ HW25/DLLNode.java
@@ -0,0 +1,84 @@
+//Stefan Tan
+//APCS2 pd1
+//HW24a -- On the DLL
+//2018-03-23
+
+/*****************************************************
+ * class DLLNode
+ * Implements a doubly-linked node,
+ * for use in lists and other collection classes.
+ * Stores data of type String
+ *****************************************************/
+
+public class DLLNode 
+{
+    private String _cargo;    //cargo may only be of type String
+    private DLLNode _nextNode, _prevNode; //pointers to next, prev DLLNodes
+
+    //YOUR IMPLEMENTATION HERE...
+    // constructor -- initializes instance vars
+    public DLLNode( String value, DLLNode next, DLLNode prev ) {
+	_cargo = value;
+	_nextNode = next;
+	_prevNode = prev;
+    }
+
+
+    //--------------v  ACCESSORS  v--------------
+    public String getCargo() { return _cargo; }
+
+    public DLLNode getNext() { return _nextNode; }
+
+    public DLLNode getPrev() { return _prevNode; }
+    //--------------^  ACCESSORS  ^--------------
+
+
+    //--------------v  MUTATORS  v--------------
+    public String setCargo( String newCargo ) {
+	String foo = getCargo();
+	_cargo = newCargo;
+	return foo;
+    }
+
+    public DLLNode setNext( DLLNode newNext ) {
+	DLLNode foo = getNext();
+	_nextNode = newNext;
+	return foo;
+    }
+
+    public DLLNode setPrev( DLLNode newPrev ) {
+	DLLNode foo = getPrev();
+	_prevNode = newPrev;
+	return foo;
+    }
+    //--------------^  MUTATORS  ^--------------
+
+    
+    // override inherited toString
+    public String toString() { return _cargo.toString(); }
+
+
+    /*    //main method for testing
+    public static void main( String[] args )
+    {
+
+	//Below is an exercise in creating a linked list...
+
+	//Create a node
+	DLLNode first = new DLLNode( "cat", null, null );
+
+	//Create a new node after the first
+	first.setNext( new DLLNode( "dog", null ) );
+
+	//Create a third node after the second
+	first.getNext().setNext( new DLLNode( "cow", null ) );
+
+	/* A naive list traversal, has side effects.... ??
+	   while( first != null ) {
+	   System.out.println( first );
+	   first = first.getNext();
+	   }
+	
+
+	   }//end main */
+}//end class DLLNode
