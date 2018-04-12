31b8d321efdfaacc7330d6bf7864be231616f7d6
StefanTan18 <stan3@stuy.edu>
2018-04-11 23:14:56 -0400

    added HW32

--- HW32/ALStack.java
+++ HW32/ALStack.java
@@ -1,3 +1,31 @@
-public class ALStack{
-    
+//Stefan Tan
+//APCS2 pd1
+//HW32 -- Leon Leonwood Stack
+//2018-04-11
+
+import java.util.ArrayList;
+
+public class ALStack<PANCAKE> implements Stack<PANCAKE>
+{
+    private ArrayList<PANCAKE> stack = new ArrayList<PANCAKE>();
+
+    public boolean isEmpty()
+    {
+	return stack.size() == 0;
+    }
+
+    public PANCAKE peek()
+    {
+	return stack.get(stack.size() - 1);
+    }
+
+    public PANCAKE pop()
+    {
+	return stack.remove(stack.size() - 1);
+    }
+
+    public void push(PANCAKE x)
+    {
+	stack.add(x);
+    }
 }
--- HW32/LLStack.java
+++ HW32/LLStack.java
@@ -1,3 +1,31 @@
-public class LLStack{
+//Stefan Tan
+//APCS2 pd1
+//HW32 -- Leon Leonwood Stack
+//2018-04-11
 
+import java.util.LinkedList;
+
+public class LLStack<PANCAKE> implements Stack<PANCAKE>
+{
+    private LinkedList<PANCAKE> stack = new LinkedList<PANCAKE>();
+
+    public boolean isEmpty()
+    {
+	return stack.size() == 0;
+    }
+
+    public PANCAKE peek()
+    {
+	return stack.getFirst();
+    }
+
+    public PANCAKE pop()
+    {
+	return stack.removeFirst();
+    }
+
+    public void push(PANCAKE x)
+    {
+	stack.addFirst(x);
+    }	
 }
