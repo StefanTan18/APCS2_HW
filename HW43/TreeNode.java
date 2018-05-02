ddf32968b5135925b15e4551e65284e6355db297
StefanTan18 <stan3@stuy.edu>
2018-05-01 23:32:45 -0400

    added HW43

--- HW43/TreeNode.java
+++ HW43/TreeNode.java
@@ -1,184 +1,94 @@
 /*****************************************************
- * class BST - v1:partial
- * Implementation of the BINARY SEARCH TREE abstract data type (ADT)
- *
- * A BST maintains the invariant that, for any node N with value V,
- * L<V && V<R, where L and R are node values in N's left and right
- * subtrees, respectively.
- * (Any value in a node's left subtree must be less than its value,
- *  and any value in its right subtree must be greater.)
- * This BST only holds ints (its nodes have int cargo)
+ * class TreeNode
+ * SKELETON
+ * Implements a node for use in a binary tree.
+ * Only holds int cargo.
  *****************************************************/
 
-public class BST
+public class TreeNode
 {
 
-  //instance variables / attributes of a BST:
-  TreeNode _root;
-
-  /*****************************************************
-   * default constructor
-   *****************************************************/
-  BST( )
-  {
-    _root = null;
-  }
-
-
-  /*****************************************************
-   * void insert( int )
-   * Adds a new data element to tree.
-   *****************************************************/
-  public void insert( int newVal )
-  {
-    TreeNode newNode = new TreeNode( newVal );
-
-    if ( _root == null ) {
-      _root = newNode;
-      return;
-    }
-    insert( _root, newNode );
-  }
-  //recursive helper for insert(int)
-  public void insert( TreeNode stRoot, TreeNode newNode )
-  {
-    if ( newNode.getValue() < stRoot.getValue() ) {
-      //if no left child, make newNode the left child
-      if ( stRoot.getLeft() == null )
-        stRoot.setLeft( newNode );
-      else //recurse down left subtree
-        insert( stRoot.getLeft(), newNode );
-      return;
-    }
-    else { // new val >= curr, so look down right subtree
-      //if no right child, make newNode the right child
-      if ( stRoot.getRight() == null )
-        stRoot.setRight( newNode );
-      else //recurse down right subtree
-        insert( stRoot.getRight(), newNode );
-      return;
-    }
-  }//end insert()
-
-
-
-
-  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  //~~~~~~~~~~~~~v~~TRAVERSALS~~v~~~~~~~~~~~~~~~~~~~~~
-
-  // each traversal should simply print to standard out
-  // the nodes visited, in order
-
-  //process root, recurse left, recurse right
-  public void preOrderTrav()
-  {
-    preOrderTrav( _root );
-  }
-  public void preOrderTrav( TreeNode currNode )
-  {
-    if ( currNode == null ) //stepped beyond leaf
-      return;
-    System.out.print( currNode.getValue() + " " );
-    preOrderTrav( currNode.getLeft() );
-    preOrderTrav( currNode.getRight() );
-  }
-
-  //recurse left, process root, recurse right
-  public void inOrderTrav()
-  {
-    inOrderTrav( _root );
-  }
-  public void inOrderTrav( TreeNode currNode )
-  {
-    if ( currNode == null ) //stepped beyond leaf
-      return;
-    inOrderTrav( currNode.getLeft() );
-    System.out.print( currNode.getValue() + " " );
-    inOrderTrav( currNode.getRight() );
-  }
-
-  //recurse left, recurse right, process root
-  public void postOrderTrav()
-  {
-    postOrderTrav( _root );
-  }
-  public void postOrderTrav( TreeNode currNode )
-  {
-    if ( currNode == null ) //stepped beyond leaf
-      return;
-    postOrderTrav( currNode.getLeft() );
-    postOrderTrav( currNode.getRight() );
-    System.out.print( currNode.getValue() + " "  );
-  }
-
-  //~~~~~~~~~~~~~^~~TRAVERSALS~~^~~~~~~~~~~~~~~~~~~~~~
-  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    //instance variables / attributes of a TreeNode:
+    int _cargo;        //this node's data
+    TreeNode _lt, _rt; //pointers to left, right subtrees
 
 
     /*****************************************************
-     * TreeNode search(int)
-     * returns pointer to node containing target,
-     * or null if target not found
+     * default constructor
+     * Construct a tree node with specified value, 
+     * with null left and right subtrees.
      *****************************************************/
-    TreeNode search( int target )
+    TreeNode( int initValue )
     {
-      /*** YOUR IMPLEMENTATION HERE ***/
+	_cargo = initValue;
+	_lt = null;
+	_rt = null;
+    }
+          
+
+    /*****************************************************
+     * overloaded constructor
+     * Construct tree node w specified value, left, and right subtrees.
+     *****************************************************/
+    TreeNode( int initValue, TreeNode initLeft, TreeNode initRight ) 
+    {
+	_cargo = initValue;
+	_lt = initLeft;
+	_rt = initRight;
+    }
+ 
+
+    /*****************************************************
+     * Returns the left subtree of this node.
+     *****************************************************/
+    TreeNode getLeft() 
+    {
+	return _lt;
     }
 
 
     /*****************************************************
-     * int height()
-     * returns height of this tree (length of longest leaf-to-root path)
-     * eg: a 1-node tree has height 0
+     * Returns the right subtree of this node.
      *****************************************************/
-    public int height()
+    TreeNode getRight() 
     {
-      /*** YOUR IMPLEMENTATION HERE ***/
+	return _rt;
     }
 
 
     /*****************************************************
-     * int numLeaves()
-     * returns number of leaves in tree
+     * Returns the value stored in this tree node.
      *****************************************************/
-    public int numLeaves()
+    int getValue() 
     {
-      /*** YOUR IMPLEMENTATION HERE ***/
+	return _cargo;
     }
 
 
+    /*****************************************************
+     * Sets the value of the left subtree of this node.
+     *****************************************************/
+    void setLeft( TreeNode theNewLeft ) 
+    {
+	_lt = theNewLeft;
+    }
 
 
-  //main method for testing
-  public static void main( String[] args )
-  {
-    BST arbol = new BST();
+    /*****************************************************
+     * Sets the value of the right subtree of this node.
+     *****************************************************/
+    void setRight( TreeNode theNewRight ) 
+    {
+	_rt = theNewRight;
+    }
 
-    //PROTIP: sketch state of tree after each insertion
-    //        ...BEFORE executing these.
-    arbol.insert( 4 );
-    arbol.insert( 2 );
-    arbol.insert( 5 );
-    arbol.insert( 6 );
-    arbol.insert( 1 );
-    arbol.insert( 3 );
 
-    System.out.println( "\n-----------------------------");
-    System.out.println( "pre-order traversal:" );
-    arbol.preOrderTrav();
-
-    System.out.println( "\n-----------------------------");
-    System.out.println( "in-order traversal:" );
-    arbol.inOrderTrav();
-
-    System.out.println( "\n-----------------------------");
-    System.out.println( "post-order traversal:" );
-    arbol.postOrderTrav();
-
-    System.out.println( "\n-----------------------------");
-    /*~~~~~~~~~~~~move~me~down~~~~~~~~~~~~~~~~~~~~~~
-      <<< YOUR NEW TEST CALLS HERE >>>
-      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-  }
-
+    /*****************************************************
+     * Sets the value of this tree node.
+     *****************************************************/
+    void setValue( int theNewValue ) 
+    {
+	_cargo = theNewValue;
+    }
+    
 }//end class
